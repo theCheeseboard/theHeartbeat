9ae9aeab17b7e400d7af93dc800015aaab7f1226
Victor Tran <vicr12345@gmail.com>
2019-01-08 10:40:45 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 85.2% (23 of 27 strings)
    
    Translation: theHeartbeat/theHeartbeat Desktop
    Translate-URL: http://translate.vicr123.com/projects/theheartbeat/theheartbeat-desktop/vi/

--- heartbeat/translations/vi_VN.ts
+++ heartbeat/translations/vi_VN.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" sourcelanguage="en_US">
+<TS version="2.1" sourcelanguage="en_US" language="vi_VN">
 <context>
     <name>MainWindow</name>
     <message>
@@ -8,84 +8,82 @@
         <location filename="../mainwindow.ui" line="67"/>
         <location filename="../mainwindow.cpp" line="215"/>
         <source>theHeartbeat</source>
-        <translation type="unfinished"></translation>
+        <translation>theHeartbeat</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="103"/>
         <location filename="../mainwindow.ui" line="172"/>
         <source>Overview</source>
-        <translation type="unfinished"></translation>
+        <translation>Tổng quan</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="111"/>
         <location filename="../mainwindow.ui" line="397"/>
         <source>Processes</source>
-        <translation type="unfinished"></translation>
+        <translation>Quá trình</translation>
     </message>
     <message>
         <location filename="../mainwindow.ui" line="188"/>
         <location filename="../mainwindow.ui" line="413"/>
         <location filename="../mainwindow.cpp" line="118"/>
         <source>Terminate Process</source>
-        <translation type="unfinished"></translation>
+        <translation>Chấm dứt quá trình</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="55"/>
         <source>CPU Usage</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng CPU</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="56"/>
         <source>Memory Usage</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng bộ nhớ</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="57"/>
         <source>Swap Usage</source>
-        <translation type="unfinished"></translation>
+        <translation>Sử dụng bộ nhớ tráo đổi</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="68"/>
         <source>CPU %1</source>
-        <translation type="unfinished"></translation>
+        <translation>CPU %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="119"/>
         <source>Are you sure you want to terminate these processes? You&apos;ll lose any unsaved work.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn có chắc chắn muốn chấm dứt quá trình sau đây không? Bạn sẽ mất việc chưa lưu.</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="145"/>
         <source>Send %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi %1</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="146"/>
         <source>Do you want to send %1 to these processes?</source>
-        <translation type="unfinished"></translation>
+        <translation>Bạn có chắc chắn muốn gửi %1 đến quá trình sau đây không?</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="175"/>
         <location filename="../mainwindow.cpp" line="178"/>
         <source>For %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Cho %1</translation>
     </message>
     <message numerus="yes">
         <location filename="../mainwindow.cpp" line="178"/>
         <source>%n processes</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-        </translation>
-    </message>
+        <translation><numerusform>%n quá trình</numerusform>
+        </translation></message>
     <message>
         <location filename="../mainwindow.cpp" line="182"/>
         <source>Send Signal</source>
-        <translation type="unfinished"></translation>
+        <translation>Gửi tín hiệu</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="194"/>
         <source>Terminate</source>
-        <translation type="unfinished"></translation>
+        <translation>Chấm dứt</translation>
     </message>
 </context>
 <context>
@@ -98,17 +96,17 @@
     <message>
         <location filename="../panes/minmaxpane.cpp" line="63"/>
         <source>%1 KB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 KB</translation>
     </message>
     <message>
         <location filename="../panes/minmaxpane.cpp" line="65"/>
         <source>%1 MB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 MB</translation>
     </message>
     <message>
         <location filename="../panes/minmaxpane.cpp" line="67"/>
         <source>%1 GB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 GB</translation>
     </message>
 </context>
 <context>
@@ -140,27 +138,27 @@
     <message>
         <location filename="../processes/processmodel.cpp" line="44"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation>Tên</translation>
     </message>
     <message>
         <location filename="../processes/processmodel.cpp" line="46"/>
         <source>CPU %</source>
-        <translation type="unfinished"></translation>
+        <translation>CPU %</translation>
     </message>
     <message>
         <location filename="../processes/processmodel.cpp" line="48"/>
         <source>Memory</source>
-        <translation type="unfinished"></translation>
+        <translation>Bộ nhớ</translation>
     </message>
     <message>
         <location filename="../processes/processmodel.cpp" line="50"/>
         <source>PID</source>
-        <translation type="unfinished"></translation>
+        <translation>PID</translation>
     </message>
     <message>
         <location filename="../processes/processmodel.cpp" line="95"/>
         <source>%1 KB</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 KB</translation>
     </message>
 </context>
 </TS>
