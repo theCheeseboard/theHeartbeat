80761cf2c1860026a0ff2c24eba62bee966d16fb
Parlance <parlance@vicr123.com>
2022-10-18 14:13:12 +0000

    Update Translations

---
+++ heartbeat/translations/es.ts
@@ -0,0 +1,249 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation>elLatidoDelCorazón</translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <source>theHeartbeat</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="93" />
+      <location filename="../mainwindow.ui" line="202" />
+      <source>Overview</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="119" />
+      <location filename="../mainwindow.ui" line="488" />
+      <source>Processes</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.ui" line="218" />
+      <location filename="../mainwindow.ui" line="504" />
+      <location filename="../mainwindow.cpp" line="226" />
+      <source>Terminate Process</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="67" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="97" />
+      <source>CPU Usage</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="98" />
+      <source>Memory Usage</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="99" />
+      <source>Swap Usage</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="100" />
+      <source>Network Receive</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="101" />
+      <source>Network Send</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="104" />
+      <source>CPU Temperature</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="106" />
+      <source>GPU Temperature</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="122" />
+      <source>CPU %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="227" />
+      <source>Are you sure you want to terminate these processes? You may lose any unsaved work.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="228" />
+      <location filename="../mainwindow.cpp" line="293" />
+      <source>Terminate</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="232" />
+      <location filename="../mainwindow.cpp" line="234" />
+      <location filename="../mainwindow.cpp" line="294" />
+      <source>Force Stop</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="233" />
+      <source>Are you sure you want to force these processes to stop? You'll lose any unsaved work.</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="238" />
+      <location filename="../mainwindow.cpp" line="240" />
+      <source>Send %1</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="239" />
+      <source>Do you want to send %1 to these processes?</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="274" />
+      <location filename="../mainwindow.cpp" line="277" />
+      <source>For %1</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform></numerusform>
+      </translation>
+      <location filename="../mainwindow.cpp" line="277" />
+      <source>%n processes</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../mainwindow.cpp" line="281" />
+      <source>Send Signal</source>
+    </message>
+  </context>
+  <context>
+    <name>ProcessAction</name>
+    <message>
+      <translation></translation>
+      <location filename="../processaction.ui" line="35" />
+      <source>Title</source>
+    </message>
+  </context>
+  <context>
+    <name>ProcessModel</name>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="47" />
+      <source>Name</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="49" />
+      <source>CPU %</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="51" />
+      <source>Memory</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="53" />
+      <source>PID</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="105" />
+      <source>%1 KiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="107" />
+      <source>%1 MiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="109" />
+      <source>%1 GiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="137" />
+      <source>Disk Sleep</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="139" />
+      <source>Debugging</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="141" />
+      <source>Stopped</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="143" />
+      <source>Zombie</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="145" />
+      <source>Dead</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../processes/processmodel.cpp" line="147" />
+      <source>Unknown</source>
+    </message>
+  </context>
+  <context>
+    <name>SidePane</name>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="80" />
+      <source>%1 KiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="82" />
+      <source>%1 MiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="84" />
+      <source>%1 GiB</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="88" />
+      <source>%1 KiB/s</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="90" />
+      <source>%1 MiB/s</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="92" />
+      <source>%1 GiB/s</source>
+    </message>
+    <message>
+      <translation></translation>
+      <location filename="../panes/sidepane.cpp" line="95" />
+      <source>%1 °C</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation></translation>
+      <location filename="../main.cpp" line="48" />
+      <source>System Monitor</source>
+    </message>
+  </context>
+</TS>
