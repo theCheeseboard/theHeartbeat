c95726227fe5a0badf958e44a437df96f6981c54
Parlance <parlance@vicr123.com>
2022-11-04 14:34:31 +0000

    Update Translations - he_IL
    Co-Authored-By: martysh12 <martyshkon@gmail.com>

---
+++ heartbeat/translations/he_IL.ts
@@ -0,0 +1,251 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<TS version="2.1">
+  <context>
+    <name>MainWindow</name>
+    <message>
+      <translation>theHeartbeat</translation>
+      <location filename="../mainwindow.ui" line="14" />
+      <source>theHeartbeat</source>
+    </message>
+    <message>
+      <translation>סקירה כללית</translation>
+      <location filename="../mainwindow.ui" line="93" />
+      <location filename="../mainwindow.ui" line="202" />
+      <source>Overview</source>
+    </message>
+    <message>
+      <translation>תהליכים</translation>
+      <location filename="../mainwindow.ui" line="119" />
+      <location filename="../mainwindow.ui" line="488" />
+      <source>Processes</source>
+    </message>
+    <message>
+      <translation>הפסק תהליך</translation>
+      <location filename="../mainwindow.ui" line="218" />
+      <location filename="../mainwindow.ui" line="504" />
+      <location filename="../mainwindow.cpp" line="226" />
+      <source>Terminate Process</source>
+    </message>
+    <message>
+      <translation>יציאה</translation>
+      <location filename="../mainwindow.cpp" line="67" />
+      <source>Exit</source>
+    </message>
+    <message>
+      <translation>שימוש במעבד</translation>
+      <location filename="../mainwindow.cpp" line="97" />
+      <source>CPU Usage</source>
+    </message>
+    <message>
+      <translation>שימוש בזיכרון</translation>
+      <location filename="../mainwindow.cpp" line="98" />
+      <source>Memory Usage</source>
+    </message>
+    <message>
+      <translation>שימוש ב-Swap</translation>
+      <location filename="../mainwindow.cpp" line="99" />
+      <source>Swap Usage</source>
+    </message>
+    <message>
+      <translation>רשת - קלט</translation>
+      <location filename="../mainwindow.cpp" line="100" />
+      <source>Network Receive</source>
+    </message>
+    <message>
+      <translation>רשת - פלט</translation>
+      <location filename="../mainwindow.cpp" line="101" />
+      <source>Network Send</source>
+    </message>
+    <message>
+      <translation>טמפרטורת המעבד</translation>
+      <location filename="../mainwindow.cpp" line="104" />
+      <source>CPU Temperature</source>
+    </message>
+    <message>
+      <translation>טמפרטורת מעבד הגרפיקה</translation>
+      <location filename="../mainwindow.cpp" line="106" />
+      <source>GPU Temperature</source>
+    </message>
+    <message>
+      <translation>מעבד %1</translation>
+      <location filename="../mainwindow.cpp" line="122" />
+      <source>CPU %1</source>
+    </message>
+    <message>
+      <translation>האם אתה באמת רוצה להפסיק את התהליכים האלה? אתה עלול לאבד עבודה שלא נשמרה.</translation>
+      <location filename="../mainwindow.cpp" line="227" />
+      <source>Are you sure you want to terminate these processes? You may lose any unsaved work.</source>
+    </message>
+    <message>
+      <translation>הפסק</translation>
+      <location filename="../mainwindow.cpp" line="228" />
+      <location filename="../mainwindow.cpp" line="293" />
+      <source>Terminate</source>
+    </message>
+    <message>
+      <translation>הפסק בכוח</translation>
+      <location filename="../mainwindow.cpp" line="232" />
+      <location filename="../mainwindow.cpp" line="234" />
+      <location filename="../mainwindow.cpp" line="294" />
+      <source>Force Stop</source>
+    </message>
+    <message>
+      <translation>האם אתה באמת רוצה להפסיק את התהליכים האלה בכוח? אתה תאבד עבודה שלא נשמרה.</translation>
+      <location filename="../mainwindow.cpp" line="233" />
+      <source>Are you sure you want to force these processes to stop? You'll lose any unsaved work.</source>
+    </message>
+    <message>
+      <translation>שלח %1</translation>
+      <location filename="../mainwindow.cpp" line="238" />
+      <location filename="../mainwindow.cpp" line="240" />
+      <source>Send %1</source>
+    </message>
+    <message>
+      <translation>האם אתה רוצה לשלוח %1 לתהליכים האלה?</translation>
+      <location filename="../mainwindow.cpp" line="239" />
+      <source>Do you want to send %1 to these processes?</source>
+    </message>
+    <message>
+      <translation>עבור %1</translation>
+      <location filename="../mainwindow.cpp" line="274" />
+      <location filename="../mainwindow.cpp" line="277" />
+      <source>For %1</source>
+    </message>
+    <message numerus="yes">
+      <translation>
+        <numerusform>תהליך %n</numerusform>
+        <numerusform>%n תהליכים</numerusform>
+        <numerusform>%n תהליכים</numerusform>
+      </translation>
+      <location filename="../mainwindow.cpp" line="277" />
+      <source>%n processes</source>
+    </message>
+    <message>
+      <translation>שלח אות</translation>
+      <location filename="../mainwindow.cpp" line="281" />
+      <source>Send Signal</source>
+    </message>
+  </context>
+  <context>
+    <name>ProcessAction</name>
+    <message>
+      <translation>כותרת</translation>
+      <location filename="../processaction.ui" line="35" />
+      <source>Title</source>
+    </message>
+  </context>
+  <context>
+    <name>ProcessModel</name>
+    <message>
+      <translation>שם</translation>
+      <location filename="../processes/processmodel.cpp" line="47" />
+      <source>Name</source>
+    </message>
+    <message>
+      <translation>CPU %</translation>
+      <location filename="../processes/processmodel.cpp" line="49" />
+      <source>CPU %</source>
+    </message>
+    <message>
+      <translation>זיכרון</translation>
+      <location filename="../processes/processmodel.cpp" line="51" />
+      <source>Memory</source>
+    </message>
+    <message>
+      <translation>PID</translation>
+      <location filename="../processes/processmodel.cpp" line="53" />
+      <source>PID</source>
+    </message>
+    <message>
+      <translation>%1 KiB</translation>
+      <location filename="../processes/processmodel.cpp" line="105" />
+      <source>%1 KiB</source>
+    </message>
+    <message>
+      <translation>%1 MiB</translation>
+      <location filename="../processes/processmodel.cpp" line="107" />
+      <source>%1 MiB</source>
+    </message>
+    <message>
+      <translation>%1 GiB</translation>
+      <location filename="../processes/processmodel.cpp" line="109" />
+      <source>%1 GiB</source>
+    </message>
+    <message>
+      <translation>שינת דיסק</translation>
+      <location filename="../processes/processmodel.cpp" line="137" />
+      <source>Disk Sleep</source>
+    </message>
+    <message>
+      <translation>איתור באגים</translation>
+      <location filename="../processes/processmodel.cpp" line="139" />
+      <source>Debugging</source>
+    </message>
+    <message>
+      <translation>הופסק</translation>
+      <location filename="../processes/processmodel.cpp" line="141" />
+      <source>Stopped</source>
+    </message>
+    <message>
+      <translation>זומבי</translation>
+      <location filename="../processes/processmodel.cpp" line="143" />
+      <source>Zombie</source>
+    </message>
+    <message>
+      <translation>מת</translation>
+      <location filename="../processes/processmodel.cpp" line="145" />
+      <source>Dead</source>
+    </message>
+    <message>
+      <translation>לא ידוע</translation>
+      <location filename="../processes/processmodel.cpp" line="147" />
+      <source>Unknown</source>
+    </message>
+  </context>
+  <context>
+    <name>SidePane</name>
+    <message>
+      <translation>%1 KiB</translation>
+      <location filename="../panes/sidepane.cpp" line="80" />
+      <source>%1 KiB</source>
+    </message>
+    <message>
+      <translation>%1 MiB</translation>
+      <location filename="../panes/sidepane.cpp" line="82" />
+      <source>%1 MiB</source>
+    </message>
+    <message>
+      <translation>%1 GiB</translation>
+      <location filename="../panes/sidepane.cpp" line="84" />
+      <source>%1 GiB</source>
+    </message>
+    <message>
+      <translation>%1 KiB/s</translation>
+      <location filename="../panes/sidepane.cpp" line="88" />
+      <source>%1 KiB/s</source>
+    </message>
+    <message>
+      <translation>%1 MiB/s</translation>
+      <location filename="../panes/sidepane.cpp" line="90" />
+      <source>%1 MiB/s</source>
+    </message>
+    <message>
+      <translation>%1 GiB/s</translation>
+      <location filename="../panes/sidepane.cpp" line="92" />
+      <source>%1 GiB/s</source>
+    </message>
+    <message>
+      <translation>%1 °C</translation>
+      <location filename="../panes/sidepane.cpp" line="95" />
+      <source>%1 °C</source>
+    </message>
+  </context>
+  <context>
+    <name>main</name>
+    <message>
+      <translation>צג מערכת</translation>
+      <location filename="../main.cpp" line="48" />
+      <source>System Monitor</source>
+    </message>
+  </context>
+</TS>
